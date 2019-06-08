4d249e62583a5def2a88613dbfeddf1efd99085c
Rodrigo Dantas <rodrigodc07@gmail.com>
2019-06-08 10:01:30 -0300

    Remove comentarios do netbeans

--- src/UERJ/output/MessageSenderService.java
+++ src/UERJ/output/MessageSenderService.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package UERJ.output;
 
 import UERJ.Message;
 
-/**
- *
- * @author LCC
- */
 public interface MessageSenderService {
     public void send(Message message);
 }
--- src/UERJ/output/MulticastSocketServer.java
+++ src/UERJ/output/MulticastSocketServer.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package UERJ.output;
 
 import UERJ.Message;
@@ -12,10 +7,6 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 
-/**
- *
- * @author LCC
- */
 public class MulticastSocketServer implements MessageSenderService{
     
     private void multicast(Message message) throws IOException {
